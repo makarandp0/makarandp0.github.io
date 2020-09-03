279b7351700371e235e753a25ed6f474f30c0492
Makarand Patwardhan <mpatwardhan@twilio.com>
2020-09-02 23:19:55 -0700

    chrome sdp demo

---
+++ chromesdpdemo/demo.js
@@ -0,0 +1,52 @@
+/* eslint-disable sort-imports */
+/* eslint-disable no-undefined */
+/* eslint-disable no-console */
+
+// demonstrates https://bugs.webkit.org/show_bug.cgi?id=212780
+// On IOS Mobile Safari (13.4)
+// Step 1: Create audio track
+// Step 2: Render local and remote (webrtc) track
+// Step 3: Switch to youtube or some other app that uses audio - and play something
+// Step 4: Go back to Safari, and notice that remote track stays in paused state.
+
+import createButton from '../jsutilmodules/button.js';
+import generateAudioTrack from '../jsutilmodules/syntheticaudio.js';
+import generateVideoTrack from '../jsutilmodules/syntheticvideo.js';
+
+const logDiv = document.getElementById('log');
+const demoDiv = document.getElementById('demo');
+const sdpView = document.getElementById('sdp');
+
+
+let pc = null;
+export function demo() {
+
+  createButton('Step 1: Create PeerConnection', demoDiv,  () => {
+    pc = new RTCPeerConnection({ 'iceServers': [], 'sdpSemantics': 'unified-plan' });
+  });
+
+  createButton('Add Audio', demoDiv,  () => {
+    const syntheticTrack = generateAudioTrack();
+    pc.addTrack(syntheticTrack);
+  });
+
+
+  createButton('Add Video', demoDiv,  () => {
+    const canvas = document.createElement('canvas');
+    const syntheticTrack = generateVideoTrack(canvas, 'Yo');
+    pc.addTrack(syntheticTrack);
+  });
+
+  createButton('Step 4. Create Offer', demoDiv, async () => {
+    const offer = await pc.createOffer();
+    const sdp = offer.sdp;
+    console.log(sdp);
+    sdpView.value = sdp;
+  });
+
+  createButton('clear sdp', demoDiv, () => {
+    sdpView.value = '';
+  });
+
+}
+
